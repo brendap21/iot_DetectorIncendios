9acf8181fb798b469b8159d60828eafaee57d7b0
brendap21 <bpna16100224@gmail.com>
2026-05-05 20:09:59 -0600

    Congfiguring firebase.js to make it work in the deploy for production

--- firebase.js
+++ firebase.js
@@ -1,25 +1,17 @@
 require("dotenv").config();
-
 const admin = require("firebase-admin");
-
-if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-    throw new Error("No existe GOOGLE_APPLICATION_CREDENTIALS en .env");
-}
-
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-
-console.log("PROJECT ID JSON:", serviceAccount.project_id);
-console.log("CLIENT EMAIL:", serviceAccount.client_email);
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        projectId: serviceAccount.project_id
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+        })
     });
 }
 
 const db = admin.firestore();
 
 console.log("Firebase conectado correctamente");
-
 module.exports = db;
